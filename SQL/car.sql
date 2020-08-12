d6595158004980d00de711a6a64619f10c311e02
逐梦无痕 <64113334+291663174@users.noreply.github.com>
2020-08-12 16:07:13 +0800

    Add files via upload

---
+++ SQL/car.sql
@@ -0,0 +1,2 @@
+INSERT INTO `car`(`Car_owner_name`, `Car_num`, `Car_type`, `Production_type`, `Production_date`, `Useful_date`, `Car_color`, `Car_miles`) VALUES ('张三', '闽A-55555', '', '', '', '', '', '');
+INSERT INTO `car`(`Car_owner_name`, `Car_num`, `Car_type`, `Production_type`, `Production_date`, `Useful_date`, `Car_color`, `Car_miles`) VALUES ('李四', '闽A-66666', '小型车辆', '国产', '', '', '灰色', '');
---
+++ SQL/car_owner.sql
@@ -0,0 +1,2 @@
+INSERT INTO `car_owner`(`Car_owner_name`, `Car_owner_tel`, `Car_owner_address`) VALUES ('张三', '13909238498', '');
+INSERT INTO `car_owner`(`Car_owner_name`, `Car_owner_tel`, `Car_owner_address`) VALUES ('李四', '13676228422', '');
